427712a8449a7d632658934ee848bc5fac5bc41c
Swayambhu <swayambhu43@gmail.com>
2023-03-08 22:51:50 -0500

    learnt about path parameters

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class Name(Enum):
+    swayabhu = "Swayambhu"
+    Omkar = "Omkar"
+    Aditya = "Aditya"
+    
+    
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
@@ -9,3 +16,11 @@ async def root():
 @app.get("/items/{items_id}")
 async def get_item(items_id: int):
     return {"item_id": items_id}
+
+@app.get("/model/{model_name}")
+async def get_person(model_name: Name):
+    return {"model_name": model_name}
+
+@app.get("/files/{files_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
